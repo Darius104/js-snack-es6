20e067ce9f4484cc362ac1b575c06fb662d7d139
Darius104 <comandarius@gmail.com>
2021-11-29 17:07:07 +0100

    forEach

---
+++ Snack 3/js/script.js
@@ -0,0 +1,29 @@
+// Dato l'array di nomi:
+// const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+// e dati due numeri min e max (min più piccolo di max).
+// Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
+// Eseguiamo questo esercizio prima con forEach e poi con filter.
+
+
+const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+
+const numMin = parseInt(prompt('Inserisci un numero "numMin:"'));
+const numMax = parseInt(prompt('Inserisci un numero "numMax:"'));
+
+let newArray = [];
+
+if (numMin < numMax){
+    myArray.forEach((element, index, array) => {
+        if ( index > numMin && index < numMax ){
+            newArray.push(element);
+        }
+    });
+
+}else{
+    alert('numMin non può essere più grande di numMax');
+}
+
+console.log(newArray);
+
+
+
