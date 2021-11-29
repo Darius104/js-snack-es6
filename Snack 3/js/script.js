b156cb1a1ea8ca3afc56cd22fc0cfabece9c0fdf
Darius104 <comandarius@gmail.com>
2021-11-29 17:20:45 +0100

    filter Done

--- Snack 3/js/script.js
+++ Snack 3/js/script.js
@@ -5,25 +5,44 @@
 // Eseguiamo questo esercizio prima con forEach e poi con filter.
 
 
+//----------
+// forEach
+//----------
+
+
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
 
 const numMin = parseInt(prompt('Inserisci un numero "numMin:"'));
 const numMax = parseInt(prompt('Inserisci un numero "numMax:"'));
 
-let newArray = [];
+// let newArray = [];
 
-if (numMin < numMax){
-    myArray.forEach((element, index, array) => {
-        if ( index > numMin && index < numMax ){
-            newArray.push(element);
+// if (numMin < numMax){
+//     myArray.forEach((element, index, array) => {
+//         if ( index > numMin && index < numMax ){
+//             newArray.push(element);
+//         }
+//     });
+
+// }else{
+//     alert('numMin non può essere più grande di numMax');
+// }
+
+// console.log(newArray);
+
+
+//----------
+// filter
+//----------
+
+const newFilterArray = myArray.filter((element, index, array) => {
+    if( numMin < numMax ){
+        if(index > numMin && index < numMax){
+            return element;
         }
-    });
-
-}else{
-    alert('numMin non può essere più grande di numMax');
-}
-
-console.log(newArray);
-
-
+    }else{
+        alert('numMin non può essere più grande di numMax');
+    }
+});
+console.log(newFilterArray);
 
